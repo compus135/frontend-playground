d188fbec21fff0b37f371354c6f49e405306bda0
compus135 <compus135@163.com>
2024-06-06 18:46:45 +0800

    fix:缓存响应结果

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import App from "./whyUseQuery/Demo04RaceCondition";
+import App from "./whyUseQuery/Demo05RaceCondition";
 
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
---
+++ src/whyUseQuery/Demo05RaceCondition.jsx
@@ -0,0 +1,36 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { doGetTodosByName } from "../services/index";
+
+const Demo03RaceCondition = () => {
+  const [todos, setTodos] = useState([]);
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    // 先请求的后返回
+    // 先返回的应该设置,后返回的不应该再设置了
+    // 如果上一个请求还没返回,就发起下一个请求,上一个请求的结果就应该忽略掉
+    let ignore = false;
+    doGetTodosByName(name).then((res) => {
+      if (!ignore) {
+        setTodos(res);
+      }
+    });
+    return () => {
+      ignore = true;
+    };
+  }, [name]);
+
+  return (
+    <div>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Demo03RaceCondition;
